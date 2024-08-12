98e4352eb1a9765ae3a3579f67174843e36a0373
Nikunj0014 <119851905+Nikunj0014@users.noreply.github.com>
2024-08-12 15:06:04 +0530

    ok

--- db.js
+++ db.js
@@ -7,8 +7,8 @@ const connectDB = async () => {
    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 30000,
-      socketTimeoutMS: 30000,
+      // serverSelectionTimeoutMS: 30000,
+      // socketTimeoutMS: 30000,
     });
     console.log('Connected to MongoDBCluster');
   } catch (err) {
