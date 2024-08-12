0438d3f346beee37e04d862f0a1ca04e4ae80022
Nikunj0014 <119851905+Nikunj0014@users.noreply.github.com>
2024-08-12 22:34:34 +0530

    settime

--- db.js
+++ db.js
@@ -2,18 +2,23 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 mongoose.set('strictQuery', false); // Optional, depending on your Mongoose configuration
 console.log("Url",process.env.MONGO_URI)
-const connectDB = async () => {
-  try {
-   await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 60000,
-      socketTimeoutMS: 60000,
-    });
-    console.log('Connected to MongoDBCluster');
-  } catch (err) {
-    console.error('Failed to connect to MongoDB', err);
-    process.exit(1); // Exit the process with failure
+const connectDB = async (retries = 5) => {
+  while (retries) {
+    try {
+      await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 60000,
+        socketTimeoutMS: 60000,
+      });
+      console.log('Connected to MongoDBCluster');
+      break; // Exit loop if successful
+    } catch (err) {
+      console.error(`Failed to connect to MongoDB. Retries left: ${retries}`, err);
+      retries -= 1;
+      if (retries === 0) process.exit(1); // Exit process if no retries left
+      await new Promise(res => setTimeout(res, 5000)); // Wait 5 seconds before retrying
+    }
   }
 };
 
